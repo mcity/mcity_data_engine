d178bcc3cc86555fe0a53dfbb1b620d2b855b6e2
Daniel Bogdoll <dbogdoll@umich.edu>
2024-09-26 15:06:21 -0400

    Computing similarities and uniqueness

--- brain.py
+++ brain.py
@@ -5,14 +5,17 @@ import fiftyone.zoo as foz
 import re
 import os
 from pathlib import Path
-import json
-import numpy as np
 import pickle
+
+import logging
+
+from config import NUM_WORKERS
 
 
 def compute_embeddings(dataset, dataset_info, embedding_model_names, seed=0):
     """
     Computes embeddings for a given dataset using UMAP, t-SNE, and PCA methods.
+    https://docs.voxel51.com/user_guide/brain.html#brain-embeddings-visualization
 
     Args:
         dataset: The dataset for which embeddings are to be computed.
@@ -21,6 +24,8 @@ def compute_embeddings(dataset, dataset_info, embedding_model_names, seed=0):
         dict: A dictionary containing the computed embeddings for 'umap', 'tsne', and 'pca'.
     """
 
+    logging.info("Computing embeddings.")
+
     dataset_name = dataset_info["name"]
     embeddings_root = "./datasets/embeddings/" + dataset_name + "/"
     Path(embeddings_root).mkdir(parents=True, exist_ok=True)
@@ -28,8 +33,11 @@ def compute_embeddings(dataset, dataset_info, embedding_model_names, seed=0):
     # Use V51 pre-defined dim. reduction methods
     dim_reduction_methods = list(fob.brain_config.visualization_methods.keys())
     dim_reduction_methods.remove("manual")
-    embeddings = {}
+    embeddings_models = {}
+    embeddings_vis = {}
 
+    # TODO: Support loading models outside of the model zoo
+    v51_model_zoo = foz.list_zoo_models()
     for model_name in embedding_model_names:
         model = foz.load_zoo_model(model_name)
 
@@ -39,11 +47,11 @@ def compute_embeddings(dataset, dataset_info, embedding_model_names, seed=0):
             )
             if os.path.exists(embedding_file_name):
                 with open(embedding_file_name, "rb") as f:
-                    model_embeddings = pickle.load(f)
+                    embeddings_models[model_name] = pickle.load(f)
             else:
-                model_embeddings = dataset.compute_embeddings(model=model)
+                embeddings_models[model_name] = dataset.compute_embeddings(model=model)
                 with open(embedding_file_name, "wb") as f:
-                    pickle.dump(model_embeddings, f)
+                    pickle.dump(embeddings_models[model_name], f)
 
             for method in dim_reduction_methods:
                 key = re.sub(r"[\W-]+", "_", model_name + "_" + method)
@@ -52,28 +60,65 @@ def compute_embeddings(dataset, dataset_info, embedding_model_names, seed=0):
                 if os.path.exists(vis_file_name):
                     with open(vis_file_name, "rb") as f:
                         points = pickle.load(f)
-                    embeddings[key] = fob.compute_visualization(
+                    embeddings_vis[key] = fob.compute_visualization(
                         dataset,
                         method=method,
-                        embeddings=model_embeddings,
                         points=points,
                         seed=seed,
                         brain_key=key,
+                        num_workers=NUM_WORKERS,
                     )
                 else:
-                    embeddings[key] = fob.compute_visualization(
+                    embeddings_vis[key] = fob.compute_visualization(
                         dataset,
                         method=method,
-                        embeddings=model_embeddings,
+                        embeddings=embeddings_models[model_name],
                         seed=seed,
                         brain_key=key,
+                        num_workers=NUM_WORKERS,
                     )
 
                     # Save VisualizationResults object
                     with open(vis_file_name, "wb") as f:
-                        pickle.dump(embeddings[key].points, f)
-                    # embeddings[key].write_json(vis_file_name)
-    else:
-        model_embeddings = None
+                        pickle.dump(embeddings_vis[key].points, f)
+        else:
+            logging.warning("Model " + model_name + " does not provide embeddings.")
 
-    return embeddings, model_embeddings
+    return embeddings_vis, embeddings_models
+
+
+def compute_similarity(dataset, embeddings_models):
+    # https://docs.voxel51.com/user_guide/brain.html#similarity
+
+    logging.info("Computing similarities.")
+
+    similarities = {}
+    for embedding_name in embeddings_models:
+        embedding = embeddings_models[embedding_name]
+        key = re.sub(r"[\W-]+", "_", embedding_name) + "_simil"
+
+        similarities[key] = fob.compute_similarity(
+            dataset, embeddings=embedding, brain_key=key, num_workers=NUM_WORKERS
+        )
+
+    return similarities
+
+
+def compute_unique_images(similarities, embeddings_vis, num_of_unique=500):
+    # https://docs.voxel51.com/user_guide/brain.html#cifar-10-example
+
+    logging.info("Computing unique images.")
+    unique_images = {}
+
+    for sim_key in similarities:
+        embeddings_vis_subset = {
+            key: embedding
+            for key, embedding in embeddings_vis.items()
+            if sim_key[:-6] in key  # Remove _simil suffix
+        }
+        for embedding_vis in embeddings_vis_subset.values():
+            similarities[sim_key].find_unique(num_of_unique)
+            plot = similarities[sim_key].visualize_unique(visualization=embedding_vis)
+            plot.show(height=800, yaxis_scaleanchor="x")
+
+        return similarities
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import yaml
-import logging
 import time
 
+from colorlog import ColoredFormatter
+import logging
+
 from data_loader.data_loader import *
-from brain import compute_embeddings
+from brain import compute_embeddings, compute_similarity, compute_unique_images
+
+# Configure logging
+formatter = ColoredFormatter(
+    "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    },
+    secondary_log_colors={},
+    style="%",
+)
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        handler,  # Log to console with colors
+        logging.FileHandler("dataengine.log"),  # Log to a file without colors
+    ],
 )
 
 
@@ -17,18 +43,21 @@ def main():
         main_config = yaml.safe_load(f)
 
     # Load the selected dataset
+    logging.info(f"Available V51 datasets: {fo.list_datasets()}")
     dataset_info = load_dataset_info(main_config["selected_dataset"])
     if dataset_info:
-        loader_function = dataset_info.get("loader")
+        loader_function = dataset_info.get("loader_fct")
         dataset = globals()[loader_function](dataset_info)
     else:
-        print("No valid dataset name provided in config.yaml")
+        logging.error("No valid dataset name provided in config.yaml")
 
     # Compute model embeddings
     embedding_model_names = main_config.get("v51_embedding_models")
-    embeddings, model_embeddings = compute_embeddings(
+    embeddings_vis, embeddings_models = compute_embeddings(
         dataset, dataset_info, embedding_model_names
     )
+    similarities = compute_similarity(dataset, embeddings_models)
+    similarities = compute_unique_images(similarities, embeddings_vis)
 
     # Launch V51 session
     time_stop = time.time()
