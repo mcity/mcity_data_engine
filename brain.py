c5e950907c6daa5ad7a4e055a7240fc7ea02409b
Daniel Bogdoll <dbogdoll@umich.edu>
2024-10-03 16:31:52 -0400

    Enhance documentation and improve code clarity with detailed docstrings and README updates

--- brain.py
+++ brain.py
@@ -15,6 +15,10 @@ import logging
 
 from config import NUM_WORKERS
 
+"""
+Implementing Voxel51 brain methods.
+"""
+
 
 class Brain:
     def __init__(self, dataset, dataset_info, embeddings_path="./datasets/embeddings/"):
@@ -43,7 +47,24 @@ class Brain:
         }
 
     def compute_embeddings(self, embedding_model_names):
-        # Use V51 pre-defined dim. reduction methods
+        """
+        Computes and stores embeddings for the given list of embedding model names. Uses V51 pre-defined dim. reduction methods.
+
+        This method performs the following steps:
+        1. Retrieves the list of pre-defined dimensionality reduction methods.
+        2. Iterates over the provided embedding model names.
+        3. Checks if each model is part of the V51 model zoo.
+        4. Loads or computes embeddings for each model.
+        5. Saves the computed embeddings to disk.
+        6. Computes and stores visualizations for the embeddings using various dimensionality reduction methods.
+
+        Parameters:
+        embedding_model_names (list): A list of model names for which embeddings need to be computed.
+
+        Raises:
+        Warning: If a model is not part of the V51 model zoo or does not provide embeddings.
+        """
+
         dim_reduction_methods = list(fob.brain_config.visualization_methods.keys())
         dim_reduction_methods.remove("manual")
 
@@ -136,9 +157,28 @@ class Brain:
                     )
 
     def compute_similarity(self):
-        # https://docs.voxel51.com/user_guide/brain.html#similarity
-        # Indexes dataset by similarity
-        # Calculates cosine distance for the embeddings without dimensionality reduction
+        """
+        Computes the similarity of embeddings for the dataset.
+
+        This method indexes the dataset by similarity and calculates the cosine
+        distance for the embeddings without dimensionality reduction. It iterates
+        over the available embedding models and computes or loads the similarity
+        results for each model.
+
+        The similarity results are stored in the `self.similarities` dictionary
+        with keys derived from the embedding model names.
+
+        References:
+            - Voxel51 Brain Similarity Documentation:
+              https://docs.voxel51.com/user_guide/brain.html#similarity
+
+        Attributes:
+            self.embeddings_models (dict): A dictionary of embedding models.
+            self.brains (dict): A dictionary to store brain results.
+            self.similarities (dict): A dictionary to store similarity results.
+            self.dataset (Dataset): The dataset object to compute similarities on.
+
+        """
 
         for embedding_name in tqdm(
             self.embeddings_models, desc="Computing similarities"
@@ -158,7 +198,35 @@ class Brain:
                 )
 
     def compute_representativeness(self, threshold=0.99):
-        # https://docs.voxel51.com/brain.html#image-representativeness
+        """
+        Computes the representativeness of frames in the dataset using specified
+        embedding models and methods.
+
+        Args:
+            threshold (float, optional): The threshold value for selecting
+                representative frames. Defaults to 0.99.
+
+        This method iterates over the embedding models and computes the
+        representativeness using different methods. The results are stored in
+        the dataset, and frames that meet the threshold criteria are updated
+        with a specific field and value from the brain taxonomy.
+
+        The method uses the following representativeness computation methods:
+            - "cluster-center"
+            - "cluster-center-downweight"
+
+        The computed representativeness is stored in the dataset under keys
+        generated from the embedding model names and methods. If the key
+        already exists in the brains attribute, the existing similarities
+        are loaded from the dataset.
+
+        The method also updates the dataset samples that meet the threshold
+        criteria by setting a specific field to a value defined in the brain
+        taxonomy and saving the changes.
+
+        References:
+            - https://docs.voxel51.com/brain.html#image-representativeness
+        """
 
         field = self.brain_taxonomy["field"]
         value = self.brain_taxonomy["value_compute_representativeness"]
@@ -193,10 +261,28 @@ class Brain:
                     sample.save()
 
     def compute_unique_images_greedy(self, perct_unique=0.01):
-        # https://docs.voxel51.com/user_guide/brain.html#cifar-10-example
-        # find_unique(n) uses a greedy algorithm that, for a given n,
-        # finds a subset of your dataset whose embeddings are as far as possible
-        # from each other (= maximizes k=1 neighbor distance)
+        """
+        Computes a subset of unique images from the dataset using a greedy algorithm.
+
+        This method identifies a subset of images whose embeddings are as far apart as possible,
+        maximizing the k=1 neighbor distance. The percentage of unique images to be found is
+        specified by the `perct_unique` parameter.
+
+        Args:
+            perct_unique (float): The percentage of unique images to find in the dataset.
+                      Default is 0.01 (1%).
+
+        Notes:
+            - The method checks if any sample in the dataset already has the label indicating
+              uniqueness. If such samples exist, the method does nothing.
+            - If no such samples exist, the method iterates over the similarities and computes
+              unique images using the `find_unique` method of each similarity object.
+            - The unique images are then tagged with a specific field and value and saved back
+              to the dataset.
+
+        References:
+            - Voxel51 Brain documentation: https://docs.voxel51.com/user_guide/brain.html#cifar-10-example
+        """
 
         sample_count = len(self.dataset.view())
         num_of_unique = perct_unique * sample_count
@@ -219,9 +305,23 @@ class Brain:
                     sample.save()
 
     def compute_unique_images_deterministic(self, threshold=0.99):
-        # https://docs.voxel51.com/api/fiftyone.brain.html#fiftyone.brain.compute_uniqueness
-        # compute_uniqueness() computes a deterministic uniqueness score
-        # for each sample in [0, 1] (= weighted k-neighbors distances for each sample)
+        """
+        Computes a deterministic uniqueness score for each sample in the dataset
+        and updates the dataset with the computed uniqueness values. Weighted k-neighbors distances for each sample.
+
+        This method iterates over the embeddings models, computes the uniqueness
+        score for each embedding, and updates the dataset with the computed scores.
+        Samples with uniqueness scores greater than or equal to the specified
+        threshold are marked with a specific field and value.
+
+        Args:
+            threshold (float, optional): The threshold value for determining
+            unique samples. Samples with uniqueness scores greater than or
+            equal to this threshold will be marked. Default is 0.99.
+
+        References:
+            - https://docs.voxel51.com/api/fiftyone.brain.html#fiftyone.brain.compute_uniqueness
+        """
 
         field = self.brain_taxonomy["field"]
         value = self.brain_taxonomy["value_compute_uniqueness"]
@@ -254,6 +354,20 @@ class Brain:
         pass
 
     def compute_similar_images(self, dist_threshold=0.03, neighbour_count=3):
+        """
+        Computes and assigns similar images based on a distance threshold and neighbour count.
+
+        This method checks if samples have already assigned fields and if not, it finds unique views
+        and assigns neighbours based on similarity. It iterates through the similarities and updates
+        the dataset with the computed values.
+
+        Parameters:
+        dist_threshold (float): The distance threshold to consider for similarity. Default is 0.03.
+        neighbour_count (int): The number of neighbours to consider for each sample. Default is 3.
+
+        Returns:
+        None
+        """
         field = self.brain_taxonomy["field"]
         field_neighbour_distance = "distance"
 
--- utils/selector.py
+++ utils/selector.py
@@ -2,13 +2,34 @@ from fiftyone import ViewField as F
 
 
 def select_random(dataset, n_samples, seed=0):
-    # Pick random number of samples
+    """
+    Select a random subset of samples from the dataset.
+
+    Parameters:
+    dataset (Dataset): The dataset from which to select samples.
+    n_samples (int): The number of samples to select.
+    seed (int, optional): The seed for the random number generator. Default is 0.
+
+    Returns:
+    Dataset: A dataset containing the randomly selected samples.
+    """
 
     random_view = dataset.take(n_samples, seed=seed)
     return random_view
 
 
 def select_by_class(dataset, classes_in, classes_out):
+    """
+    Filters a dataset based on inclusion and exclusion of specified classes.
+
+    Args:
+        dataset: The dataset to be filtered.
+        classes_in (str or list of str): Classes to include in the filtered dataset.
+        classes_out (str or list of str): Classes to exclude from the filtered dataset.
+
+    Returns:
+        A filtered view of the dataset based on the specified inclusion and exclusion conditions.
+    """
     incl_conditions = None
     excl_conditions = None
 
