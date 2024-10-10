b3ac0ada677461d36809af02053f31585e63f47c
Daniel Bogdoll <dbogdoll@umich.edu>
2024-10-10 11:36:35 -0400

    Pdoc Introduction

---
+++ __init__.py
@@ -0,0 +1,21 @@
+r'''
+# Mcity Data Engine
+
+The Mcity Data Engine is an essential tool in the Mcity makerspace for transportation innovators making AI algorithms and seeking actionable data insights through machine learning.
+
+Learn more: https://mcity.umich.edu/what-we-do/data-for-ai/
+# Overview
+
+Our Data Engine allows researchers to efficiently query large volumes of data – directing Mcity’s transportation data collection networks with specific tasks to produce high-quality artificial intelligence models for development.
+
+Using the power of AI, the Mcity Data Engine provides our member companies and researchers with fine-tuned data sets for mobility applications through a continuously-improving loop that uses data and labels to optimize and improve the performance of AI algorithms, delivering only the specific data wanted.
+
+The Mcity Data Engine is the first tool of its kind to be made available to Mcity Members, including early stage companies. The Mcity Data Engine:
+
+- Provides access to a large compute cluster tailored to AI training for mobility applications
+- Efficiently collects, processes and labels vast networks of Mcity data
+- Interfaces with Mcity data and models to build AI-powered solutions
+- Develops large, diverse, annotated training data sets for vehicle testing
+- Boosts training data sets with computer simulations to create and add synthetic data
+- Runs at computer speeds when automated, operating 24/7
+- Serves as building block for other mobility transportation projects
