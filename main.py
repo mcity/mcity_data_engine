2accd885082a47dbbec10c4303803b20f4119854
Daniel Bogdoll <dbogdoll@umich.edu>
2024-10-10 17:58:26 -0400

    Add tag to W&B logging and remove wandb_run parameter from model training

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def main():
             pbar.set_description("Training/Loading Anomalib model " + MODEL_NAME)
 
             ano_dec = Anodec(dataset, dataset_info, model_name=MODEL_NAME)
-            ano_dec.train_and_export_model(wandb_run=wandb_run)
+            ano_dec.train_and_export_model()
             ano_dec.run_inference()
             # ano_dec.eval_v51()
             ano_dec.unlink_symlinks()
