15f934f851e1fed0391d231aa0b9255f0069f452
Daniel Bogdoll <dbogdoll@umich.edu>
2024-10-11 14:16:53 -0400

    Add logging for WandB config in anomalib_run.py

--- wandb_runs/anomalib_run.py
+++ wandb_runs/anomalib_run.py
@@ -10,6 +10,7 @@ def main():
     logging.warning(wandb.run)
     wandb.init()
     config = wandb.config
+    logging.warning(config)
     dataset_name = config["overrides"]["run_config"]["v51_dataset_name"]
     dataset_info = load_dataset_info(dataset_name)
     if dataset_info:
