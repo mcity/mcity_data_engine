80fc88836d333fab0adff845b206544880c88b9e
Daniel Bogdoll <dbogdoll@umich.edu>
2024-10-24 17:22:45 -0400

    Update session_v51.py to specify wait duration in session launch

--- session_v51.py
+++ session_v51.py
@@ -3,4 +3,4 @@ import fiftyone as fo
 from config.config import V51_ADDRESS, V51_PORT
 
 session = fo.launch_app(address=V51_ADDRESS, port=V51_PORT)
-session.wait()  # (-1) forever
+session.wait(-1)  # (-1) forever
