2e1a79b8737804f9befd840a81ca6f96fbab9805
Daniel Bogdoll <dbogdoll@umich.edu>
2024-10-29 15:24:52 -0400

    Refactor session launch into main function for better structure

--- session_v51.py
+++ session_v51.py
@@ -1,6 +1,11 @@
 import fiftyone as fo
-
 from config.config import V51_ADDRESS, V51_PORT
 
-session = fo.launch_app(address=V51_ADDRESS, port=V51_PORT)
-session.wait(-1)  # (-1) forever
+
+def main():
+    session = fo.launch_app(address=V51_ADDRESS, port=V51_PORT)
+    session.wait(-1)  # (-1) forever
+
+
+if __name__ == "__main__":
+    main()
