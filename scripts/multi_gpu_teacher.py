d88662843a7ede320ef7965f374c4f1558f4a0ab
Daniel Bogdoll <dbogdoll@umich.edu>
2024-11-19 18:30:40 -0500

    Teacher runs in single-mode, multi-processing not working

--- scripts/multi_gpu_teacher.py
+++ scripts/multi_gpu_teacher.py
@@ -8,73 +8,77 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+from torchvision.transforms.functional import to_pil_image
+
 from torch.utils.data import DataLoader, Subset, Dataset
 from torchvision.models import resnet18, vgg16
 from concurrent.futures import ProcessPoolExecutor, wait
 import time
+import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 from utils.data_loader import FiftyOneTorchDatasetCOCO
 
+from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
 
 # Function to perform inference with a specific model on a specific GPU
-def run_inference(device, model_name, dataloader, index_run):
-    torch.cuda.set_device(device)
-    
-    #Tensorboard logging
-    writer = SummaryWriter(log_dir=f"logs/tensorboard/teacher_zeroshot_dev/{index_run}")
+def run_inference(device, model_name, dataloader, batch_size, index_run):
+    print(f"Process ID: {os.getpid()}, Device: {device}, Model: {model_name}")
+    run_successfull = True
+    try:
+        torch.cuda.set_device(device)
+        object_classes = ["pedestrian", "vehicle", "cyclist"]
 
-    # Load the model
-    if model_name == "resnet":
-        model = resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
-    elif model_name == "vgg":
-        model = vgg16(weights=torchvision.models.VGG16_Weights.DEFAULT)
-    else:
-        raise ValueError("Invalid model name. Choose 'ResNet' or 'VGG'.")
-    
-    # Send model to the specified GPU
-    model = model.to(device)
-    model.eval()
+        #Tensorboard logging
+        log_dir_root = "../logs/tensorboard/teacher_zeroshot"
+        experiment_name = f"{model_name}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+        log_directory = os.path.join(log_dir_root, experiment_name)
+        writer = SummaryWriter(log_dir=log_directory)
 
-    criterion = nn.CrossEntropyLoss()  # Example criterion to calculate loss
-    total_loss = 0.0
-    correct = 0
-    total = 0
+        # Load the model
+        if model_name == "omlab/omdet-turbo-swin-tiny-hf":
+            processor = AutoProcessor.from_pretrained(model_name)
+            model = AutoModelForZeroShotObjectDetection.from_pretrained(model_name)
+            batch_classes = [object_classes] * batch_size
+        else:
+            raise ValueError("Invalid model name. Choose 'ResNet' or 'VGG'.")
+        
+        model = model.to(device)
 
-    print(f"Running {model_name} on GPU {device}...")
+        with torch.no_grad():
+            for step, (images, labels) in enumerate(dataloader):
+                time_start = time.time()
 
-    with torch.no_grad():
-        for step, (inputs, labels) in enumerate(dataloader):
-            time_start = time.time()
+                if len(images) < batch_size:
+                    batch_classes = [object_classes] * len(images)
 
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            total_loss += loss.item()
+                #OmDet
+                images = [to_pil_image(image) for image in images]
+                inputs = processor(
+                            text=batch_classes,
+                            images=images,
+                            return_tensors="pt",
+                        ).to(device)
 
-            _, predicted = outputs.max(1)
-            correct += predicted.eq(labels).sum().item()
-            total += labels.size(0)
-            time_end = time.time()
-            duration = time_end - time_start
-            batches_per_second = 1/duration
-            writer.add_scalar(f'inference/batches_per_second', batches_per_second, step)
+                outputs = model(**inputs)
+                
+                time_end = time.time()
+                duration = time_end - time_start
+                batches_per_second = 1/duration
+                frames = len(images)
+                frames_per_second = batches_per_second * frames
+                writer.add_scalar(f'inference/frames_per_second', frames_per_second, step)
 
-    print(f"{model_name} on GPU {device}: Accuracy: {100 * correct / total:.2f}%, Loss: {total_loss:.4f}")
-    writer.close()
+    except Exception as e:
+        print(f"Error in Process {os.getpid()}: {e}")
+        run_successfull = False
 
-# Load CIFAR-100 dataset
-def load_cifar100():
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    dataset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
-    return dataset
-
-
+    finally:
+        writer.close()
+        return run_successfull
 
 if __name__ == "__main__":
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"   # Warning from huggingface/tokenizers
     torch.cuda.empty_cache()
     
     #Hardware configuration
@@ -83,14 +87,22 @@ if __name__ == "__main__":
     print(f"CPU count: {n_cpus}. GPU count: {n_gpus}")
 
     # Dataset and configurations
-    dataset_v51 = fo.load_dataset("dbogdollumich/mcity_fisheye_v51")
+    dataset_v51_orig = fo.load_dataset("dbogdollumich/mcity_fisheye_v51")
+    dataset_v51 = dataset_v51_orig.take(101)
+
     dataset = FiftyOneTorchDatasetCOCO(dataset_v51)
     batch_size=8
 
+    dataloader_test = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=24, pin_memory=True, collate_fn=lambda batch: list(zip(*batch)))
+
+    # Test 'run_inference'
+    run_successfull = run_inference("cuda:0", "omlab/omdet-turbo-swin-tiny-hf", dataloader_test, batch_size, 0)
+    if run_successfull == False:
+        print("Test run not successful")
+
     # Define models and dataset splits per model (1 = whole dataset)
     models_splits_dict = {
-        "resnet": 1,
-        "vgg": 2
+        "omlab/omdet-turbo-swin-tiny-hf": 2
     }
 
     runs_dict = {}
@@ -110,7 +122,7 @@ if __name__ == "__main__":
             # Generate torch subsets
             split_size = split_size + (leftover_samples if split_id == n_splits - 1 else 0)
             subset = Subset(dataset, range(split_index_start, split_index_start + split_size))
-            dataloader = DataLoader(subset, batch_size=batch_size, shuffle=False, num_workers=24)
+            dataloader = DataLoader(subset, batch_size=batch_size, shuffle=False, num_workers=24, pin_memory=True, collate_fn=lambda batch: list(zip(*batch)))
             split_index_start = split_size
             
             # Add entry to runs_dict
@@ -123,16 +135,19 @@ if __name__ == "__main__":
             }
             run_counter += 1
     
+    print(runs_dict)
+
     # Create processes for each GPU (model + dataset split)
     with ProcessPoolExecutor() as executor:
         time_start = time.time()
         futures = []
         for run_id, run_metadata in runs_dict.items():
+            print(f"Launch job {run_id}")
             index = run_id
             device  = f"cuda:{run_id}"
             model_name = run_metadata["model_name"]
             dataloader = run_metadata["dataloader"]
-            future = executor.submit(run_inference, device, model_name, dataloader, index)
+            future = executor.submit(run_inference, device, model_name, dataloader, batch_size, index)
             futures.append(future)
 
         # Wait for all tasks to complete
