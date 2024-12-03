9a3b68b445076e786221aff6e35b6e88a8920d31
Daniel Bogdoll <dbogdoll@umich.edu>
2024-12-03 11:50:39 -0500

    Copy code from challenge, logging

--- workflows/teacher.py
+++ workflows/teacher.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import os
 import re
+import subprocess
 import time
 from difflib import get_close_matches
 from functools import partial
@@ -9,12 +10,13 @@ from typing import List, Union
 
 import fiftyone as fo
 import numpy as np
+import psutil
 import torch
+import torch.multiprocessing as mp
 from datasets import Split
 from PIL import Image
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torch.utils.tensorboard import SummaryWriter
-from torchvision import transforms
 from torchvision.transforms.functional import to_pil_image
 from tqdm import tqdm
 from transformers import (AutoConfig, AutoModelForObjectDetection,
@@ -245,12 +247,320 @@ class ZeroShotInferenceCollateFn:
                 text=self.batch_classes,
                 images=images,
                 return_tensors="pt",
-                padding=True
+                padding=True    # Allow for differently sized images
             )
 
             return inputs, labels, target_sizes, self.batch_classes
         except Exception as e:
-            print(f"Error in collate function of DataLoader: {e}")
+            logging.error(f"Error in collate function of DataLoader: {e}")
+
+class ZeroShotPrediction:
+    # Helper functions
+    def _get_gpu_compute_modes(self):
+        """Gets the compute modes of all GPUs."""
+        try:
+            result = subprocess.run(["nvidia-smi", "--query-gpu=compute_mode", "--format=csv,noheader"], 
+                                    capture_output=True, text=True, check=True)
+            modes = result.stdout.strip().split("\n")
+            return modes
+        except subprocess.CalledProcessError as e:
+            logging.error("Error running nvidia-smi:", e)
+            return []
+
+    def _distribute_cpu_cores(self, cpu_cores, n_processes):
+        n_cores = len(cpu_cores)
+
+        chunk_size = n_cores // n_processes
+        remainder = n_cores % n_processes
+
+        cpu_cores_per_process = []
+        start = 0
+        for i in range(n_processes):
+            # Determine the end index for this chunk
+            end = start + chunk_size + (1 if i < remainder else 0)
+            cpu_cores_per_process.append(cpu_cores[start:end])
+            start = end
+
+        return cpu_cores_per_process
+
+    def _get_batch_classes(self, hf_model_config_name, object_classes, batch_size):
+        if hf_model_config_name == "GroundingDinoConfig":
+                classes = " . ".join(object_classes) + " . "
+                batch_classes = [classes] * batch_size
+        elif hf_model_config_name == "OmDetTurboConfig":
+            batch_classes = [object_classes] * batch_size
+        elif hf_model_config_name == "Owlv2Config" or hf_model_config_name == "OwlViTConfig":
+            batch_classes = object_classes * batch_size
+        else:
+            logging.error("Invalid model name")
+        
+        return batch_classes
+
+    # Worker functions
+    def process_outputs_worker(self, dataset_name, object_classes, results_queue, inference_finished, queue_warning_threshold=5):
+        logging.info(f"Process ID: {os.getpid()}. Results processing process started")    
+        dataset_v51 = fo.load_dataset(dataset_name)
+        processing_successful = None
+        while True:
+            if results_queue.qsize() > queue_warning_threshold:
+                logging.warning(f"Queue size of {results_queue.qsize()}. Consider increasing number of post-processing workers.")
+            # Exit only when inference is finished and the queue is empty
+            if inference_finished.value and results_queue.empty():
+                logging.info(f"Post-processing worker {os.getpid()} has finished all outputs.")
+                break
+            # Process results from the queue if available
+            if not results_queue.empty():
+                try:
+                    result = results_queue.get_nowait()
+                    processing_successful = self.process_outputs(dataset_v51, result, object_classes)
+                except Exception as e:
+                    continue
+            else:
+                continue
+        return processing_successful    # Return last processing status
+    
+    def gpu_worker(self, gpu_id, cpu_cores, task_queue, results_queue, dataset, object_classes, event, post_processing_finished, set_cpu_affinity=False):
+        # Set CPU
+        if set_cpu_affinity:
+            # Allow only certain CPU cores
+            psutil.Process().cpu_affinity(cpu_cores)
+        logging.info(f"Available CPU cores: {psutil.Process().cpu_affinity()}")
+        max_n_cpus = len(cpu_cores)
+        torch.set_num_threads(max_n_cpus)
+        
+        # Set GPU
+        logging.info(f"GPU {gpu_id}: {torch.cuda.get_device_name(gpu_id)}")
+        device = torch.device(f"cuda:{gpu_id}")
+
+        # Set WandB directory
+        root_log_dir = "logs/tensorboard/teacher_zeroshot"  # FIXME
+        
+        run_successful = None
+        with torch.cuda.device(gpu_id):
+            while True:
+                if post_processing_finished.value and task_queue.empty():
+                    # Keep running until post-processing is done
+                    break
+
+                if task_queue.empty():
+                    # Signalize that worker is finished
+                    event.set()
+
+                if not task_queue.empty():
+                    # Perform task
+                    try:
+                        task_metadata = task_queue.get(timeout=5)  # Timeout to prevent indefinite blocking
+                    except Exception:
+                        break  # Exit if no more tasks
+                    run_successful = self.model_inference(task_metadata, device, dataset, object_classes, results_queue, root_log_dir)
+                    logging.info(f"Worker for GPU {gpu_id} finished run successful: {run_successful}")
+                
+                else:
+                    continue
+
+        logging.info(f"Worker for GPU {gpu_id} shuts down.")
+        return run_successful
+
+    # Functionality functions
+    def model_inference(self, metadata: dict, device: str, dataset: torch.utils.data.Dataset, object_classes: list, results_queue: Union[queue.Queue, mp.Queue], root_log_dir: str, num_workers: int = 4, persistent_workers: bool = False, prefetch_factor: int = 4, detection_threshold: int = 0.2):
+        writer = None
+        run_successful = True
+        model = None
+
+        try:
+            # Metadata
+            run_id = metadata["run_id"]
+            model_name = metadata["model_name"]
+            dataset_name = metadata["dataset_name"]
+            is_subset = metadata["is_subset"]
+            batch_size = metadata["batch_size"]
+            
+            logging.info(f"Process ID: {os.getpid()}, Run ID: {run_id}, Device: {device}, Model: {model_name}")        
+
+            # Load the model
+            print(f"Loading model {model_name}")
+            processor = AutoProcessor.from_pretrained(model_name)
+            model = AutoModelForZeroShotObjectDetection.from_pretrained(model_name)
+            model = model.to(device)
+            model.eval()
+            hf_model_config = AutoConfig.from_pretrained(model_name)
+            hf_model_config_name = type(hf_model_config).__name__
+            logging.info(f"Loaded model type {hf_model_config_name}")
+            
+            batch_classes = self._get_batch_classes(hf_model_config_name=hf_model_config_name, object_classes=object_classes, batch_size=batch_size)
+
+            # Dataloader
+            logging.info("Generating dataloader")
+            if is_subset:
+                chunk_index_start = metadata["chunk_index_start"]
+                chunk_index_end = metadata["chunk_index_end"]
+                logging.info(f"Length of dataset: {len(dataset)}")
+                logging.info(f"Subset start index: {chunk_index_start}")
+                logging.info(f"Subset stop index: {chunk_index_end}")
+                dataset = Subset(dataset, range(chunk_index_start, chunk_index_end))
+            
+            zero_shot_inference_preprocessing = ZeroShotInferenceCollateFn(hf_model_config_name=hf_model_config_name, hf_processor=processor, object_classes=object_classes, batch_size=batch_size, batch_classes=batch_classes)
+            dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, persistent_workers=persistent_workers, pin_memory=True, prefetch_factor=prefetch_factor, collate_fn=zero_shot_inference_preprocessing)
+            
+            # Logging
+            experiment_name = f"{model_name}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{device}"
+            log_directory = os.path.join(root_log_dir, dataset_name, experiment_name)
+            writer = SummaryWriter(log_dir=log_directory)
+
+            # Inference Loop
+            print("Starting inference")
+            n_processed_images = 0
+            for inputs, labels, target_sizes, batch_classes in dataloader:
+                time_start = time.time()
+                n_images = len(labels)
+                inputs = inputs.to(device)           # TODO Add non_blocking=True after HF PR is merged: https://github.com/huggingface/transformers/pull/34883
+
+                with torch.amp.autocast("cuda"), torch.inference_mode():
+                    outputs = model(**inputs)
+
+                result = {
+                    "inputs": inputs,
+                    "outputs": outputs,
+                    "processor": processor,
+                    "target_sizes": target_sizes,
+                    "labels": labels,
+                    "model_name": model_name,
+                    "hf_model_config_name": hf_model_config_name,
+                    "batch_classes": batch_classes
+                }
+
+                results_queue.put(result)
+
+                time_end = time.time()
+                duration = time_end - time_start
+                batches_per_second = 1 / duration
+                frames_per_second = batches_per_second * n_images
+                n_processed_images += n_images
+                writer.add_scalar(f'inference/frames_per_second', frames_per_second, n_processed_images)
+            
+
+        except Exception as e:
+            logging.error(f"Error in Process {os.getpid()}: {e}")
+            run_successful = False
+
+        finally:
+            logging.info(f"Finished run")
+            del model
+            torch.cuda.empty_cache()
+            if writer:
+                writer.close()
+            return run_successful
+        
+    def process_outputs(self, dataset_v51, result, object_classes, detection_threshold=0.2):
+        processing_successful = True
+        try:
+            inputs = result["inputs"]
+            outputs = result["outputs"]
+            target_sizes = result["target_sizes"]
+            labels = result["labels"]
+            model_name = result["model_name"]
+            hf_model_config_name = result["hf_model_config_name"]
+            batch_classes = result["batch_classes"]
+            processor = result["processor"]
+
+            # Processing output
+            if hf_model_config_name == "GroundingDinoConfig":
+                results = processor.post_process_grounded_object_detection(
+                    outputs,
+                    inputs.input_ids,
+                    box_threshold=detection_threshold,
+                    text_threshold=detection_threshold,
+                )
+            elif hf_model_config_name in ["Owlv2Config", "OwlViTConfig"]:
+                results = processor.post_process_object_detection(
+                    outputs=outputs,
+                    threshold=detection_threshold,
+                    target_sizes=target_sizes,
+                )
+            elif hf_model_config_name == "OmDetTurboConfig":
+                results = processor.post_process_grounded_object_detection(
+                    outputs,
+                    classes=batch_classes,
+                    score_threshold=detection_threshold,
+                    nms_threshold=detection_threshold,
+                    target_sizes=target_sizes,
+                )
+            else:
+                logging.error(f"Invalid model name: {hf_model_config_name}")
+
+            if not len(results) == len(target_sizes) == len(labels):
+                logging.error(f"Lengths of results, target_sizes, and labels do not match: {len(results)}, {len(target_sizes)}, {len(labels)}")
+            for result, size, target in zip(results, target_sizes, labels):
+                boxes, scores = result["boxes"], result["scores"]
+
+                img_height = size[0]
+                img_width = size[1]
+
+                if "labels" in result:
+                    labels = result["labels"]
+                elif "classes" in result:  # OmDet deviates from the other models
+                    labels = result["classes"]
+
+                detections = []
+                for box, score, label in zip(boxes, scores, labels):
+                    processing_successful = True
+                    if hf_model_config_name == "GroundingDinoConfig":
+                        # Outputs do not comply with given labels
+                        # Grounding DINO outputs multiple pairs of object boxes and noun phrases for a given (Image, Text) pair
+                        # There can be either multiple labels per output ("bike van"), incomplete ones ("motorcyc"), or broken ones ("##cic")
+                        processed_label = label.split()[0]          # Assume first output is the best output
+                        if processed_label in object_classes:
+                            label = processed_label
+                            top_left_x = box[0].item()
+                            top_left_y = box[1].item()
+                            box_width = (box[2] - box[0]).item()
+                            box_height = (box[3] - box[1]).item()
+                        else:
+                            logging.warning(f"Skipped detection with {hf_model_config_name} due to unclear output: {label}")
+                            processing_successful = False
+
+                    elif hf_model_config_name in [
+                        "Owlv2Config",
+                        "OwlViTConfig",
+                    ]:
+                        label = object_classes[label]   # Label is ID
+                        top_left_x = box[0].item() / img_width
+                        top_left_y = box[1].item() / img_height
+                        box_width = (box[2].item() - box[0].item()) / img_width
+                        box_height = (box[3].item() - box[1].item()) / img_height
+
+                    elif hf_model_config_name == "OmDetTurboConfig":
+                        top_left_x = box[0].item() / img_width
+                        top_left_y = box[1].item() / img_height
+                        box_width = (box[2].item() - box[0].item()) / img_width
+                        box_height = (box[3].item() - box[1].item()) / img_height
+
+                    if processing_successful:   # Skip GroundingDinoConfig labels that could not be processed
+                        detection = fo.Detection(
+                            label=label,
+                            bounding_box=[
+                                top_left_x,
+                                top_left_y,
+                                box_width,
+                                box_height,
+                            ],
+                            confidence=score.item(),
+                        )
+                        detection["bbox_area"] = (
+                            detection["bounding_box"][2] * detection["bounding_box"][3]
+                        )
+                        detections.append(detection)
+
+                # Attach label to V51 dataset
+                pred_key = re.sub(r"[\W-]+", "_", "pred_" + model_name)
+                sample = dataset_v51[target["image_id"]]
+                sample[pred_key] = fo.Detections(detections=detections)
+                sample.save()
+        except Exception as e:
+            logging.error(f"Error in processing outputs: {e}")
+            processing_successful = False
+        finally:
+            return processing_successful
 
 class ZeroShotTeacher:
     def __init__(self):
