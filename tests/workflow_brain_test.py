2d12dbdd3856c5a261dd8622214a7d6a36b4ae59
Daniel Bogdoll <dbogdoll@umich.edu>
2024-12-05 18:23:33 -0500

    Improved brain tests

--- tests/workflow_brain_test.py
+++ tests/workflow_brain_test.py
@@ -16,7 +16,7 @@ def dataset_v51():
     dataset_name = "mcity_fisheye_v51_brain_test"
     try:
         dataset = load_from_hub(
-            repo_id=dataset_name_hub, max_samples=50, name=dataset_name
+            repo_id=dataset_name_hub, max_samples=200, name=dataset_name
         )
     except:
         dataset = fo.load_dataset(dataset_name)
@@ -24,23 +24,20 @@ def dataset_v51():
     return dataset
 
 def test_brain(dataset_v51):
-    MODEL_NAME = "dinov2-vitl14-torch"
-    dataset_info = load_dataset_info("mcity_fisheye_2000")
+    MODEL_NAME = "mobilenet-v2-imagenet-torch"
+    dataset_info = load_dataset_info("mcity_fisheye_2000")  # Use loader for actual dataset
+    dataset_info["name"] = "mcity_fisheye_v51_brain_test"   # Update with test name for local tests where both exist
     v51_keys = workflow_brain(dataset_v51, dataset_info, MODEL_NAME)
 
     embedding_key = v51_keys["embedding"]
-    similiarity_key = v51_keys["similarity"]
     uniqueness_key = v51_keys["uniqueness"]
 
     embeddings = dataset_v51.values(embedding_key)
-    similarity = dataset_v51.values(similiarity_key)
     uniqueness = dataset_v51.values(uniqueness_key)
 
     print(embeddings)
-    print(similarity)
     print(uniqueness)
 
     # Assert the results are not empty and have the correct length
     assert len(embeddings) == len(dataset_v51), "Embeddings length does not match dataset length"
-    assert len(similarity) == len(dataset_v51), "Similarity length does not match dataset length"
     assert len(uniqueness) == len(dataset_v51), "Uniqueness length does not match dataset length"
