4c69234f8354f860b87b63973928c013aa13b868
Daniel Bogdoll <dbogdoll@umich.edu>
2025-02-10 14:38:23 -0500

    Anomaly Detection Test

--- tests/workflow_anomaly_detection_test.py
+++ tests/workflow_anomaly_detection_test.py
@@ -1,6 +1,12 @@
 import fiftyone as fo
 import pytest
+from fiftyone import ViewField as F
 from fiftyone.utils.huggingface import load_from_hub
+
+from config.config import ACCEPTED_SPLITS
+from main import workflow_anomaly_detection
+from utils.anomaly_detection_data_preparation import AnomalyDetectionDataPreparation
+from utils.dataset_loader import _post_process_dataset
 
 
 @pytest.fixture
@@ -12,15 +18,47 @@ def dataset_v51():
     dataset_name = "fisheye8k_v51_anomaly_test"
     try:
         dataset = load_from_hub(
-            repo_id=dataset_name_hub, max_samples=200, name=dataset_name
+            repo_id=dataset_name_hub, max_samples=50, name=dataset_name
         )
     except:
         dataset = fo.load_dataset(dataset_name)
     assert dataset is not None, "Failed to load or create the FiftyOne dataset"
+
+    dataset = _post_process_dataset(dataset)
     return dataset
 
 
 def test_anomaly_detection(dataset_v51):
-    pass
-    # TODO Run and test PADIM model, it does not require training
-    # Check if any masks exist with values != 0
+
+    prep_config = {"location": "cam3", "rare_classes": ["Bus"]}
+
+    data_preparer = AnomalyDetectionDataPreparation(
+        dataset_v51, "fisheye8k", config=prep_config
+    )
+    run_config = {
+        "model_name": "Padim",
+        "image_size": [265, 265],
+        "batch_size": 1,
+        "epochs": 1,
+        "early_stop_patience": 1,
+        "data_root": data_preparer.export_root,
+    }
+
+    eval_metrics = ["AUPR", "AUROC"]
+
+    dataset_info = {"name": "fisheye8k"}
+
+    workflow_anomaly_detection(
+        data_preparer.dataset_ano_dec,
+        dataset_info,
+        eval_metrics,
+        run_config,
+    )
+
+    # Select all samples that are considered anomalous
+    view_anomalies = data_preparer.dataset_ano_dec.filter_labels(
+        "pred_anomaly_Padim", F("label") == "anomaly"
+    )
+    n_samples_selected = len(view_anomalies)
+    print(f"{n_samples_selected} samples with anomalies found")
+    assert n_samples_selected != 0, "No samples were selected through anomaly detection"
