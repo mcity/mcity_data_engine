7585ab63e4b607c1d716098f5871bcf741fe3e6f
Daniel Bogdoll <dbogdoll@umich.edu>
2025-02-12 16:36:59 -0500

    Test case data reduction

--- tests/workflow_embedding_selection_test.py
+++ tests/workflow_embedding_selection_test.py
@@ -15,7 +15,7 @@ def dataset_v51():
     dataset_name = "fisheye8k_v51_brain_test"
     try:
         dataset = load_from_hub(
-            repo_id=dataset_name_hub, max_samples=200, name=dataset_name
+            repo_id=dataset_name_hub, max_samples=100, name=dataset_name
         )
     except:
         dataset = fo.load_dataset(dataset_name)
