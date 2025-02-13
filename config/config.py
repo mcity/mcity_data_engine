aa364b6d6e296dc858d093d94b7096db43d30b0b
Daniel Bogdoll <dbogdoll@umich.edu>
2025-02-13 09:23:55 -0500

    Removed dinov2-vitl14-torch from config due to high GPU memory requirements

--- config/config.py
+++ config/config.py
@@ -1,12 +1,12 @@
 import os
 
 # Selection from WORKFLOWS
-SELECTED_WORKFLOW = ["auto_labeling"]
+SELECTED_WORKFLOW = ["embedding_selection"]
 
 # Choose from config/datasets.yaml
 SELECTED_DATASET = {
     "name": "fisheye8k",
-    "n_samples": 100,  # 'None' (full dataset) or 'int' (subset of the dataset)
+    "n_samples": None,  # 'None' (full dataset) or 'int' (subset of the dataset)
 }
 
 PERSISTENT = True  # If V51 database is stored
@@ -30,46 +30,28 @@ WORKFLOWS = {
             "neighbour_count": 3,
         },
         "embedding_models": [  # Select from V51 "Embeddings" models https://docs.voxel51.com/model_zoo/models.html
-            "clip-vit-base32-torch",
+            # "clip-vit-base32-torch",
             # "open-clip-torch",
-            # "dinov2-vits14-torch",        # Issue with query IDs
-            # "dinov2-vitl14-torch",        # Issue with query IDs
-            # "dinov2-vits14-reg-torch",
-            # "dinov2-vitl14-reg-torch",    # High GPU memory requirements
+            # "dinov2-vits14-torch",
+            "dinov2-vits14-reg-torch",
             "mobilenet-v2-imagenet-torch",
-            # "resnet152-imagenet-torch",
-            # "vgg19-imagenet-torch",
-            # "classification-transformer-torch",
-            # "detection-transformer-torch",
-            # "zero-shot-detection-transformer-torch",
-            # "zero-shot-classification-transformer-torch",
+            "resnet152-imagenet-torch",
+            "vgg19-imagenet-torch",
+            "classification-transformer-torch",
+            "detection-transformer-torch",
+            "zero-shot-detection-transformer-torch",
+            "zero-shot-classification-transformer-torch",
         ],
     },
     "anomaly_detection": {
         "mode": ["train", "inference"],  # "train" and "inference" supported
-        "epochs": 1,
+        "epochs": 36,
         "early_stop_patience": 5,
         "anomalib_image_models": {  # Choose from https://anomalib.readthedocs.io/en/v1.2.0/markdown/guides/reference/models/image/index.html
-            "Padim": {"batch_size": 1, "image_size": [960, 960]},
-            "EfficientAd": {"batch_size": 1, "image_size": [960, 960]},
-            # "Draem": {"batch_size": 1, "image_size": [960, 960]},
-            "Cfa": {"batch_size": 1, "image_size": [960, 960]},
-            "Cflow": {"batch_size": 1, "image_size": [960, 960]},
-            # "Csflow": {"batch_size": 1, "image_size": [960, 960]},    # Node fc1_0 error
-            "Dfm": {"batch_size": 1, "image_size": [960, 960]},
-            "Dsr": {"batch_size": 1, "image_size": [960, 960]},
-            "Fastflow": {"batch_size": 1, "image_size": [960, 960]},
-            "ReverseDistillation": {"batch_size": 1, "image_size": [960, 960]},
-            "Rkde": {"batch_size": 1, "image_size": [960, 960]},
-            "Stfpm": {"batch_size": 1, "image_size": [960, 960]},
-            "Uflow": {
-                "batch_size": 1,
-                "image_size": [448, 448],
-            },  # Inflexible model w.r.t image size
-            # "WinClip",    # Requires language input
-            # "Dfkde",      # Has no pixel metrics
-            # "Ganomaly",   # Has no pixel metrics
-            # "Patchcore",  # Ineffiecient algorithm, cannot load whole dataset
+            "Padim": {},
+            "EfficientAd": {},
+            "Draem": {},
+            "Cfa": {},
         },
         "anomalib_eval_metrics": [  # Choose from https://anomalib.readthedocs.io/en/v1.2.0/markdown/guides/reference/metrics/index.html
             "AUPR",
@@ -92,7 +74,7 @@ WORKFLOWS = {
             "hf_models_objectdetection"
         ],  # "hf_models_objectdetection" and "custom_codetr" and "ultralytics" supported
         "n_worker_dataloader": 3,
-        "epochs": 2,
+        "epochs": 36,
         "early_stop_patience": 5,
         "early_stop_threshold": 0,
         "learning_rate": 5e-05,
@@ -136,8 +118,12 @@ WORKFLOWS = {
                 "image_size": [960, 960],
             },
             "hustvl/yolos-base": {"batch_size": 1},
-            # "PekingU/rtdetr_r50vd": {},              # Tensor size mismatch error
-            # "PekingU/rtdetr_r50vd_coco_o365": {},    # Tensor size mismatch error
+            "PekingU/rtdetr_r50vd": {
+                "batch_size": 2
+            },  # Tensor size mismatch error, maybe due to batch_size=1 https://discuss.huggingface.co/t/potential-bug-in-the-rt-detr-v2-fine-tune-script/139774
+            "PekingU/rtdetr_r50vd_coco_o365": {
+                "batch_size": 2
+            },  # Tensor size mismatch error
         },
         "custom_codetr": {
             "train_model": True,  # Set false if model file should be loaded without training
@@ -158,29 +144,29 @@ WORKFLOWS = {
         "hf_models_zeroshot_objectdetection": {
             # dataset_chunks: Number of chunks to split the dataset into for parallel processing       # batch_size
             "omlab/omdet-turbo-swin-tiny-hf": {
-                "batch_size": 64,
+                "batch_size": 1,
                 "n_dataset_chunks": 1,
             },  # RTX 4090: 64 ; H 100: 128
             "IDEA-Research/grounding-dino-tiny": {
-                "batch_size": 8,
+                "batch_size": 1,
                 "n_dataset_chunks": 1,
             },  # RTX 4090: 8 ;  H 100: 32
             # "IDEA-Research/grounding-dino-base": {"batch_size": 8, "n_dataset_chunks": 1},            # RTX 4090: 8 ;  H 100: ?
             # "google/owlvit-base-patch16": {"batch_size": 8, "n_dataset_chunks": 1},                   # RTX 4090: 8 ;  H 100: ?
             # "google/owlvit-base-patch32": {"batch_size": 8, "n_dataset_chunks": 1},                   # RTX 4090: 8 ;  H 100: ?
             "google/owlvit-large-patch14": {
-                "batch_size": 4,
+                "batch_size": 1,
                 "n_dataset_chunks": 8,
             },  # RTX 4090: 4 ;  H 100: 16
             # "google/owlv2-base-patch16": {"batch_size": 32, "n_dataset_chunks": 1},                    # RTX 4090: 8 ;  H 100: 32
             # "google/owlv2-base-patch16-ensemble": {"batch_size": 8, "n_dataset_chunks": 1},           # RTX 4090: 8 ;  H 100: ?
             "google/owlv2-base-patch16-finetuned": {
-                "batch_size": 8,
+                "batch_size": 1,
                 "n_dataset_chunks": 8,
             },  # RTX 4090: 8 ;  H 100: 16
             # "google/owlv2-large-patch14": {"batch_size": 8, "n_dataset_chunks": 8},                   # RTX 4090: 2 ;  H 100: 8
             "google/owlv2-large-patch14-ensemble": {
-                "batch_size": 2,
+                "batch_size": 1,
                 "n_dataset_chunks": 8,
             },  # RTX 4090: 2 ;  H 100: 8
             # "google/owlv2-large-patch14-finetuned": {"batch_size": 2, "n_dataset_chunks": },          # RTX 4090: 2 ;  H 100: ?
@@ -244,7 +230,7 @@ WORKFLOWS = {
 
 ACCEPTED_SPLITS = {"train", "val", "test"}
 HF_ROOT = "mcity-data-engine"  # https://huggingface.co/mcity-data-engine
-HF_DO_UPLOAD = False
+HF_DO_UPLOAD = True
 
 cpu_count = os.cpu_count()
 NUM_WORKERS = 32 if cpu_count > 32 else cpu_count
